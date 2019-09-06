8f18347ebc04ae88730e3266c22eb2d08946bad0
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-06 15:41:47 +0700

    [DELETE] Remove some scaffolding items for React.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
    .sourceMaps()
    .browserSync({
         proxy: '127.0.0.1:8000'
