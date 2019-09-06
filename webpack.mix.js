1fbd466b881bafa3c0069e2355123ff26d1b0154
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-06 21:40:00 +0700

    [UPDATE] Add LESS file to compile in Laravel Mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.react('resources/js/app.js', 'public/js')
+   .less('resources/less/app.less', 'public/css',  {
+        javascriptEnabled: true
+    })
    .sourceMaps()
    .browserSync({
         proxy: '127.0.0.1:8000'
