cbb4db2db8c387245dfb8905c57bc19da4e52e27
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-11 21:40:05 +0700

    [EDIT] Apply global style by 'styled-components'.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import './bootstrap'
 import { IndexPage } from './pages'
 import { store } from './scripts/redux';
 
+const GlobalStyle = createGlobalStyle`
+    #app {
+        min-height: 100vh;
+
+        & > .ant-layout {
+            min-height: inherit;
+        }
+    }
+`
+
 const App = () => (
     <Provider store={store}>
+        <GlobalStyle />
         <Router>
             <Route exact path="/" component={IndexPage} />
         </Router>
