08548719614a4414b4d7559847c7e88c19690ee6
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-11 21:38:07 +0700

    [CREATE] Apply state container with Redux.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './bootstrap'
 import { IndexPage } from './pages'
+import { store } from './scripts/redux';
 
 const App = () => (
-    <Router>
-        <Route exact path="/" component={IndexPage} />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Route exact path="/" component={IndexPage} />
+        </Router>
+    </Provider>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'));
