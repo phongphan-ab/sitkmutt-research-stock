11fa0c70f04326491dc5a140e27395237b3d0625
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-26 20:12:01 +0700

    [EDIT] Import matchMedia to necessary libraries.

--- resources/js/pages/IndexPage.js
+++ resources/js/pages/IndexPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Layout, Menu, Input, Icon, Breadcrumb, Typography, Avatar, Dropdown, Badge, Divider } from 'antd'
 import styled from 'styled-components'
+import 'matchmedia-polyfill/matchMedia'
+import 'matchmedia-polyfill/matchMedia.addListener'
 
 const { Header, Sider, Content, Footer } = Layout
 const { SubMenu } = Menu
