4f9dcdab3e4325952e40e4f0b0c405bb72fde62e
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-26 20:02:25 +0700

    [EDIT] Remove old search box components.

--- resources/js/pages/IndexPage.js
+++ resources/js/pages/IndexPage.js
@@ -4,40 +4,7 @@ import styled, { keyframes } from 'styled-components'
 
 const { Header, Sider, Content, Footer } = Layout
 const { SubMenu } = Menu
-const { Search } = Input
 const { Text } = Typography
-
-const NavbarSearchBox = styled(Search)`
-    input {
-        border: none;
-        background-color: rgba(255, 255, 255, .10);
-        color: rgba(255, 255, 255, 0.5);
-        transition: all .125s linear;
-
-        &:focus {
-            -webkit-box-shadow: none;
-            background-color: rgba(255, 255, 255, 1);
-            color: rgba(142, 142, 142, 1);
-            box-shadow: none;
-    
-            &::placeholder {
-                color: rgba(142, 142, 142, 1);
-            }
-
-            & ~ .ant-input-suffix > i {
-                color: rgba(142, 142, 142, 1);
-            }
-        }
-
-        &::placeholder {
-            color: rgba(255, 255, 255, 0.5);
-        }
-
-        & ~ .ant-input-suffix > i {
-            color: rgba(255, 255, 255, 0.5);
-        }
-    }
-`
 
 const RightMenuWrapper = styled.div`
     display: flex;
