61c1a2d6aeeca2f169eab6afc507cb9542c75a1e
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-28 13:28:20 +0700

    [CREATE] Make a triat for autofill UUID for model which use this trait.

---
+++ app/Traits/HasUuid.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUuid
+{
+    protected static function bootHasUuid()
+    {
+        static::creating(function ($model) {
+            $model->object_id = (string) Str::uuid();
+        });
+    }
+}
