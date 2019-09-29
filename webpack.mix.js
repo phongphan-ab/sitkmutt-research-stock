5e7c7d74ea7117f94f2c9bff7531a6a2496e5add
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-09-29 20:58:20 +0700

    [EDIT] Apply 'dotenv' to get docker container port number from .env file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 const mix = require('laravel-mix');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 mix.react('resources/js/app.js', 'public/js')
    .less('resources/less/app.less', 'public/css',  {
@@ -6,7 +9,7 @@ mix.react('resources/js/app.js', 'public/js')
     })
    .sourceMaps()
    .browserSync({
-        proxy: '127.0.0.1:8000'
+        proxy: `127.0.0.1:${process.env.DOCKER_SERVER_CONTAINER_PORT}`
    })
    .webpackConfig({
         resolve: {
