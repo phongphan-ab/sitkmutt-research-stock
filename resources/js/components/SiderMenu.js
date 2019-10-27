df92277a669040879a368f9d35c8b6a130b890e8
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-10-27 14:25:52 +0700

    [EDIT] Add link to access stock categories page on cpanel section.

--- resources/js/components/SiderMenu.js
+++ resources/js/components/SiderMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { Icon, Menu } from 'antd';
 
 const { SubMenu } = Menu
@@ -20,7 +21,7 @@ const SiderMenu = () => (
         }>
             <Menu.Item key="cpanel.users"><Icon type="user" /><span>ผู้ใช้</span></Menu.Item>
             <Menu.Item key="cpanel.stocks"><Icon type="container" /><span>พัสดุ</span></Menu.Item>
-            <Menu.Item key="cpanel.stock_categories"><Icon type="appstore" /><span>ประเภทพัสดุ</span></Menu.Item>
+            <Menu.Item key="cpanel.stock_categories"><Link to="/cpanel/stock_categories"><Icon type="appstore" /><span>ประเภทพัสดุ</span></Link></Menu.Item>
             <Menu.Item key="cpanel.stock_locations"><Icon type="bank" /><span>สถานที่เก็บพัสดุ</span></Menu.Item>
             <Menu.Item key="cpanel.stock_borrowing_manager"><Icon type="book" /><span>การยืมและการคืนพัสดุ</span></Menu.Item>
             <Menu.Item key="cpanel.reports"><Icon type="file-text" /><span>รายงาน</span></Menu.Item>
