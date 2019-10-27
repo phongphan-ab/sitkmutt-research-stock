47cba5053cb4545f22c9f9e0e12f60bb73f07f29
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-10-27 19:05:11 +0700

    [EDIT] Apply middleware with redux-thunk.

--- resources/js/scripts/redux.js
+++ resources/js/scripts/redux.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+
 import rootReducer from '~/scripts/redux/reducers';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancer(applyMiddleware(thunk))
 );
