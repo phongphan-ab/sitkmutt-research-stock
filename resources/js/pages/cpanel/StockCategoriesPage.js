19024e31bc58b48e3a797055d57a4ffec911241e
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-10-27 15:20:51 +0700

    [EDIT] Map states to props of StockCategoriesPage component.

--- resources/js/pages/cpanel/StockCategoriesPage.js
+++ resources/js/pages/cpanel/StockCategoriesPage.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { connect }  from 'react-redux'
 
-class StockCategoriesPage extends Component {   
+class StockCategoriesPageContainer extends Component {   
 
     render() {
 
     }
 }
+
+const mapStateToProps = state => ({
+    isStockCategoryAddingModalOpen: state.cpanelStockCategoryAddingModal,
+    stockCategories: state.stockCategories.data,
+    stockCategoriesLoading: state.stockCategories.loading,
+    stockCategoriesFailure: state.stockCategories.error,
+    stockCategoryModifyItem: state.stockCategoriesModifyItem
+})
+
+const StockCategoriesPage = connect(mapStateToProps)(StockCategoriesPageContainer)
 
 export default StockCategoriesPage
