8c0fca75decf9e0b58ace61fa60858f8423718a8
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-12-14 15:06:01 +0700

    [EDIT] Add DeleteStockCategory and StockCategoryFormModal components.

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,11 +1,15 @@
+import DeleteStockCategoryButton from './DeleteStockCategoryButton'
 import DrawerMenu from './DrawerMenu'
 import ErrorModal from './ErrorModal'
+import StockCategoryFormModal from './StockCategoryFormModal'
 import NavbarSearchBox from './NavbarSearchBox'
 import SiderMenu from './SiderMenu'
 
 export {
+    DeleteStockCategoryButton,
     DrawerMenu,
     ErrorModal,
+    StockCategoryFormModal,
     NavbarSearchBox,
     SiderMenu
 }
