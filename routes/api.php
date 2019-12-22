eaa9ffb0d5742f5a405f9bab22ae6d2641c3690c
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-12-22 20:05:43 +0700

    [CREATE] Create a resource controller for '/stock'

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('stock_categories', 'StockCategoriesController');
+Route::resource('stocks', 'StocksController');
