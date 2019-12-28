66b2fce4bb843abe9bc85e4936193acce8372e82
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-12-28 16:45:43 +0700

    [EDIT] Add model attribute for Stock model.

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -11,6 +11,20 @@ class Stock extends Model
 {
     use HasUuid;
 
+    public $fillable = [
+        'title',
+        'description',
+        'is_visible'
+    ];
+
+    protected $hidden = [
+        'id'
+    ];
+
+    protected $casts = [
+        'is_visible' => 'boolean',
+    ];
+
     public function stock_category() {
         return $this->belongsTo(StockCategory::class, 'category_id');
     }
