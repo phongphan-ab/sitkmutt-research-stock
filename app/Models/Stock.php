715c3eb23bcfc72f4a0284e09d93a647dcd07624
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2020-01-11 18:47:08 +0700

    [EDIT] Edit class namespace reference in Stock model.

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-use StockCategory;
+use App\Models\StockCategory;
 use App\Models\StockPicture;
 use App\Models\StockSku;
 use App\Traits\HasUuid;
