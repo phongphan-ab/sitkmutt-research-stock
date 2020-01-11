783c3c073100531b4073f0e976f26b0eaefa813b
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2020-01-11 16:07:27 +0700

    [REMOVE] Remove some configuration from minio storage.

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,6 @@ return [
         'minio' => [
             'driver' => 's3',
             'endpoint' => env('MINIO_ENDPOINT', 'http://127.0.0.1:9000'),
-            'use_path_style_endpoint' => true,
             'key' => env('MINIO_KEY'),
             'secret' => env('MINIO_SECRET'),
             'region' => env('MINIO_REGION'),
