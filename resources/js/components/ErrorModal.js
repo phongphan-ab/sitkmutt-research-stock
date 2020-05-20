8d8420b690d51308f4a3ab4217f8e0195b2c88cc
Phongphan Bunlom <phongphan.phb@gmail.com>
2020-05-20 16:22:51 +0700

    [EDIT] Call i18next instance directly instead of call from react hook.

--- resources/js/components/ErrorModal.js
+++ resources/js/components/ErrorModal.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import { Modal } from 'antd'
-import { useTranslation } from 'react-i18next'
+import i18next from 'i18next';
 
 const ErrorModal = (error, description) => {
-    const { t } = useTranslation()
     let message = null;
 
     if (error.response && error.response.data.error && error.response.data.error.code) {
         switch (error.response.data.error.code) {
             case 'ERR_STOCKSKU_REMAINING':
-                message = t('alert.messages.err_skustock_remaining')
+                message = i18next.t('alert.messages.err_skustock_remaining')
                 break;
         }
     }
 
     return Modal.error({
-        title: t('alert.title', {context: 'error'}),
+        title: i18next.t('alert.title', {context: 'error'}),
         content: <>
-            <p>{ message || description || t('alert.messages.other.description')}</p>
-            { error.response ? (<p><small>{t('alert.messages.other.http_error_code', {code: error.response.status})}</small></p>) : null}
+            <p>{ message || description || i18next.t('alert.messages.other.description')}</p>
+            { error.response ? (<p><small>{i18next.t('alert.messages.other.http_error_code', {code: error.response.status})}</small></p>) : null}
         </>,
-        okText: t('alert.button.ok')
+        okText: i18next.t('alert.button.ok')
     })
 }
 
